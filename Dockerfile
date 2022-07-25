2f1fef1ffdba85f62c0fea14e8b0def4cf700fe3
francis-devbr <francis-devbr@outlook.com>
2022-07-24 22:54:07 -0300

    first commit

---
+++ Dockerfile
@@ -0,0 +1,10 @@
+FROM adoptopenjdk/openjdk11:alpine-jre
+
+ARG JAR_FILE=target/ia-config-server-0.0.1.jar
+
+WORKDIR /opt/app
+
+COPY ${JAR_FILE} app.jar
+
+
+ENTRYPOINT ["java","-jar","app.jar"]
---
+++ src/main/java/br/com/ia/configserver/IaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.ia.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IaConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IaConfigServerApplication.class, args);
+	}
+
+}
