8db8a07e0091fe8df6b9508cf1c614fc39af20e2
francis-devbr <francis-devbr@outlook.com>
2022-07-28 16:33:27 -0300

    nome

--- src/test/java/br/com/plusgestao/configserver/ConfigServerApplicationTests.java
+++ src/test/java/br/com/plusgestao/configserver/ConfigServerApplicationTests.java
@@ -1,10 +1,10 @@
-package br.com.ia.configserver;
+package br.com.plusgestao.configserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class IaConfigServerApplicationTests {
+class ConfigServerApplicationTests {
 
 	@Test
 	void contextLoads() {
